eb608b3a31daf0571f5753b130414affc3d96d21
Jonathan Powell <jpowell.cr@gmail.com>
2021-09-06 12:35:55 -0500

    fix: change p to h1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>jpowell.dev</p>
+        <h1>jpowell.dev</h1>
       </header>
     </div>
   );
