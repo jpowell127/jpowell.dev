edc9ad66642afae39122604a6738b3abcb2054a1
Jonathan Powell <jpowell.cr@gmail.com>
2021-09-08 08:32:24 -0500

    testing github actions

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <h1>jpowell.dev</h1>
+        <p>Coming Soon</p>
       </header>
     </div>
   );
